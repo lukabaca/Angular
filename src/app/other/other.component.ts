b6228c8aeef1ab2c57319485b6fd1249cbcfedd4
lukasz.blaszczyk <lukasz.blaszczyk@exaco.pl>
2018-08-13 09:24:05 +0200

    working still on reactive forms

--- src/app/other/other.component.ts
+++ src/app/other/other.component.ts
@@ -4,6 +4,7 @@ import {FormControl} from '@angular/forms';
 import { FormGroup} from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-other',
@@ -16,7 +17,9 @@ export class OtherComponent implements OnInit {
 
     name: ['', Validators.required],
     surname: [''],
-
+    aliases: this.formBuilder.array([
+        this.formBuilder.control('')
+      ])
 });
 
 
@@ -34,8 +37,18 @@ export class OtherComponent implements OnInit {
 
   onSubmit() {
     this.submitted = true;
-    console.warn(this.personForm.value);
-    // this.people.push(this.person);
+
+    console.warn(this.personForm.value.name);
+    console.warn(this.personForm.value.surname);
+
+    const name = this.personForm.value.name;
+    const surname = this.personForm.value.surname;
+
+    this.person = new Person(1, name, surname);
+
+    this.people.push(this.person);
+
+    this.personForm.reset();
   }
 
   constructor(private formBuilder: FormBuilder) {
@@ -51,6 +64,14 @@ export class OtherComponent implements OnInit {
 
 
   get diagnostic() { return JSON.stringify(this.person); }
+
+  get aliases() {
+    return this.personForm.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+    this.aliases.push(this.formBuilder.control(''));
+  }
 
   newPerson() {
     this.person = new Person(1, '', '');
