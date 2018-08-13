4df1dbed7b357ebe63d1712f17f852780e7830b2
lukasz.blaszczyk <lukasz.blaszczyk@exaco.pl>
2018-08-13 12:31:36 +0200

    trying to do error msg

---
+++ src/app/Service/question.service.ts
@@ -0,0 +1,48 @@
+import { Injectable }       from '@angular/core';
+import {QuestionBase} from '../Model/QuestionBase';
+import {DropdownQuestion} from '../Model/QuestionDropdown';
+import {TextboxQuestion} from '../Model/QuestionTextBox';
+
+
+
+@Injectable()
+export class QuestionService {
+
+  // TODO: get from a remote source of question metadata
+  // TODO: make asynchronous
+  getQuestions() {
+
+    let questions: QuestionBase<any>[] = [
+
+      new DropdownQuestion({
+        key: 'brave',
+        label: 'Bravery Rating',
+        options: [
+          {key: 'solid',  value: 'Solid'},
+          {key: 'great',  value: 'Great'},
+          {key: 'good',   value: 'Good'},
+          {key: 'unproven', value: 'Unproven'}
+        ],
+        order: 3
+      }),
+
+      new TextboxQuestion({
+        key: 'firstName',
+        label: 'First name',
+        type: 'text',
+        value: 'Bombasto',
+        required: true,
+        order: 1
+      }),
+
+      new TextboxQuestion({
+        key: 'emailAddress',
+        label: 'Email',
+        type: 'email',
+        order: 2
+      })
+    ];
+
+    return questions.sort((a, b) => a.order - b.order);
+  }
+}
